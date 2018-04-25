515e94c9598b568e319d0275f49e3dc18f1de951
Uilian Ries <uilianries@gmail.com>
2018-04-25 20:37:01 -0300

    Use beta package on CPT
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -12,5 +12,5 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 $SUDO pip install conan --upgrade
-$SUDO pip install conan_package_tools
+$SUDO pip install conan_package_tools==0.17.0-beta2
 conan user
